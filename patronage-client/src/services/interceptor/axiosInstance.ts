40a69f23ff6fc0b39e02df4cb4b110a57be7f5ac
Andrii <bodnarandrew123@gmail.com>
2024-05-27 12:25:46 +0300

    minor corrections

--- patronage-client/src/services/interceptor/axiosInstance.ts
+++ patronage-client/src/services/interceptor/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
